aa1e58543f22542f43dc1bef59ea4a8a1d86d20d
中野 伸吾 <nakano_shingo@tenda.co.jp>
2019-05-31 16:55:06 +0900

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,85 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h3>テキストバインディング</h3>
+    <span>{{ msg }}</span>
+
+    <h3>リストレンダリング</h3>
+    <ul id="item-list">
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
+      </li>
+    </ul>
+
+    <h3>イベントハンドリング</h3>
+    <button v-on:click="onClick">実行</button>
+
+    <h3>算出プロパティ</h3>
+    <input type="text" v-model="msg">
+    <p>{{ computedMsg }}</p>
+
+    <h3>条件付きレンダリング</h3>
+    <button v-on:click="showDetail">表示</button>
+    <div v-if="detail">
+      詳細が表示されました。
+    </div>
+
+    <button v-on:click="raiseError">エラー</button>
+    <div v-bind:class="{ 'text-danger': hasError }">
+      システムからのメッセージ
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Vue from "vue";
+
+declare interface Item {
+  id: number;
+  name: string;
+}
 
 export default Vue.extend({
-  name: 'home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      msg: "Hello Vue.js!",
+      items: [
+        {
+          id: 1,
+          name: "キーボード"
+        },
+        {
+          id: 2,
+          name: "マウス"
+        }
+      ] as Item[],
+      detail: false,
+      hasError: false
+    };
   },
+  methods: {
+    onClick(): void {
+      alert(this.msg);
+    },
+    showDetail(): void {
+      this.detail = true;
+    },
+    raiseError(): void {
+      this.hasError = true;
+    }
+  },
+  computed: {
+    computedMsg(): string {
+      return this.msg.replace(/!/g, "") + " + TypeScript!";
+    }
+  }
 });
 </script>
+
+<style scoped>
+.home {
+  text-align: left;
+}
+.text-danger {
+  color: darkred;
+}
+</style>
